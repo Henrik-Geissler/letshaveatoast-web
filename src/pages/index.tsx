e0f6d0e1ab4c9116d99db58806ecfc0b7a77bb1b
Your Name <you@example.com>
2020-12-09 11:26:32 +0100

    ja

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,108 +1,40 @@
-import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/core";
-import { withUrqlClient } from "next-urql";
-import NextLink from "next/link";
-import { useState } from "react";
-import { EditDeletePostButtons } from "../components/EditDeletePostButtons";
-import { Layout } from "../components/Layout";
-import { UpdootSection } from "../components/UpdootSection";
-import { usePostsQuery, PostsQuery } from "../generated/graphql";
-import { createUrqlClient } from "../utils/createUrqlClient";
-import { withApollo } from "../utils/withApollo";
-
+import { Layout } from '../components/Layout'
+import { withApollo } from '../utils/withApollo'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 const Index = () => {
-  const { data, error, loading, fetchMore, variables } = usePostsQuery({
-    variables: {
-      limit: 15,
-      cursor: null,
-    },
-    notifyOnNetworkStatusChange: true,
-  });
+  const [data, setData] = useState({ hits: [] })
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://api.fullstack.fun/graphql?query={getToast{amount,category,firstname,lastname}}'
+      )
 
-  if (!loading && !data) {
+      setData(result.data)
+    }
+
+    fetchData()
+  }, [])
+
+  if (!data) {
     return (
       <div>
         <div>you got query failed for some reason</div>
-        <div>{error?.message}</div>
       </div>
-    );
+    )
   }
 
   return (
     <Layout>
-      {!data && loading ? (
-        <div>loading...</div>
-      ) : (
-        <Stack spacing={8}>
-          {data!.posts.posts.map((p) =>
-            !p ? null : (
-              <Flex key={p.id} p={5} shadow="md" borderWidth="1px">
-                <UpdootSection post={p} />
-                <Box flex={1}>
-                  <NextLink href="/post/[id]" as={`/post/${p.id}`}>
-                    <Link>
-                      <Heading fontSize="xl">{p.title}</Heading>
-                    </Link>
-                  </NextLink>
-                  <Text>posted by {p.creator.username}</Text>
-                  <Flex align="center">
-                    <Text flex={1} mt={4}>
-                      {p.textSnippet}
-                    </Text>
-                    <Box ml="auto">
-                      <EditDeletePostButtons
-                        id={p.id}
-                        creatorId={p.creator.id}
-                      />
-                    </Box>
-                  </Flex>
-                </Box>
-              </Flex>
-            )
-          )}
-        </Stack>
-      )}
-      {data && data.posts.hasMore ? (
-        <Flex>
-          <Button
-            onClick={() => {
-              fetchMore({
-                variables: {
-                  limit: variables?.limit,
-                  cursor:
-                    data.posts.posts[data.posts.posts.length - 1].createdAt,
-                },
-                // updateQuery: (
-                //   previousValue,
-                //   { fetchMoreResult }
-                // ): PostsQuery => {
-                //   if (!fetchMoreResult) {
-                //     return previousValue as PostsQuery;
-                //   }
-
-                //   return {
-                //     __typename: "Query",
-                //     posts: {
-                //       __typename: "PaginatedPosts",
-                //       hasMore: (fetchMoreResult as PostsQuery).posts.hasMore,
-                //       posts: [
-                //         ...(previousValue as PostsQuery).posts.posts,
-                //         ...(fetchMoreResult as PostsQuery).posts.posts,
-                //       ],
-                //     },
-                //   };
-                // },
-              });
-            }}
-            isLoading={loading}
-            m="auto"
-            my={8}
-          >
-            load more
-          </Button>
-        </Flex>
-      ) : null}
+      <ul>
+        {data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
     </Layout>
-  );
-};
+  )
+}
 
-export default withApollo({ ssr: true })(Index);
+export default withApollo({ ssr: true })(Index)
